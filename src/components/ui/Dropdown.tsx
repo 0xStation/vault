5db0e6f7027cfb1474873223c273c1eb3c809860
kristencheung <krcheung9@gmail.com>
2023-02-09 15:36:52 -0500

    remove unnecessary 'add client'

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { CheckIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import * as React from "react"
