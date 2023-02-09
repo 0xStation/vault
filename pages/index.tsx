87b5739c97260439aa353abe725bb03c54020c2d
kristencheung <krcheung9@gmail.com>
2023-02-09 14:04:26 -0500

    terminal, request, activities nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@ui/NavigationMenu"
 import { AccountNavBar } from "../src/components/core/AccountNavBar"
 
 function Page() {
-  return <AccountNavBar />
+  return (
+    <>
+      <AccountNavBar />
+      <NavigationMenu className="">
+        <NavigationMenuList className="rounded-lg border border-black">
+          <NavigationMenuItem className="border-r border-black">
+            {/* <Link href="/docs" legacyBehavior passHref> */}
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle({ position: "left" })}
+            >
+              Terminal
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Requests
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="border-l border-black">
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle({ position: "right" })}
+            >
+              Activities
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          {/* </Link> */}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
+  )
 }
 
 export default Page
