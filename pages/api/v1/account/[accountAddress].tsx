5f0bfbbbcb8d15e9b85946cbf37e56d48501f222
Conner Swenberg <cls364@cornell.edu>
2023-02-16 15:18:46 -0500

    Separate account creation API route from fetch

--- pages/api/v1/account/[accountAddress].tsx
+++ pages/api/v1/account/[accountAddress].tsx
@@ -24,39 +24,10 @@ export default async function handler(
       }
 
       if (!account) {
-        return null
+        res.statusCode = 404
+        return res.end(JSON.stringify("Account not found"))
       }
 
-      res.status(200).json(account)
-      break
-    case "PUT":
-      const { address, pfpUrl, chainId } = body
-
-      try {
-        account = await prisma.account.findFirst({
-          where: { address },
-        })
-      } catch (err) {
-        console.warn("Error finding existing account", err)
-      }
-
-      if (account) {
-        res.statusCode = 500
-        return res.end(JSON.stringify("Account already exists"))
-      }
-
-      try {
-        account = await prisma.account.create({
-          data: {
-            address,
-            chainId,
-            data: { pfpUrl },
-          },
-        })
-      } catch (err) {
-        res.statusCode = 500
-        return res.end(JSON.stringify(`Internal error: ${err}`))
-      }
       res.status(200).json(account)
       break
     default:
---
+++ pages/api/v1/account/index.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import prisma from "../../../../prisma"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method, body } = req
+
+  let account
+  switch (method) {
+    case "PUT":
+      const { address, pfpUrl, chainId } = body
+
+      try {
+        account = await prisma.account.findFirst({
+          where: { address },
+        })
+      } catch (err) {
+        console.warn("Error finding existing account", err)
+      }
+
+      if (account) {
+        res.statusCode = 500
+        return res.end(JSON.stringify("Account already exists"))
+      }
+
+      try {
+        account = await prisma.account.create({
+          data: {
+            address,
+            chainId,
+            data: { pfpUrl },
+          },
+        })
+      } catch (err) {
+        res.statusCode = 500
+        return res.end(JSON.stringify(`Internal error: ${err}`))
+      }
+      res.status(200).json(account)
+      break
+    default:
+      res.setHeader("Allow", ["GET", "PUT"])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
