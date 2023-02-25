5758bbf218a36600f1f9e5c4d19a6f41da3658c5
conner <38736612+symmtry@users.noreply.github.com>
2023-02-24 22:32:09 -0800

    Add comment editing (#49)
    * Add comment editing
    
    * Add edit permissions to comment creator

--- src/components/core/ActivityItem.tsx
+++ src/components/core/ActivityItem.tsx
@@ -1,29 +1,68 @@
-import { ActivityVariant } from "@prisma/client"
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@ui/Dropdown"
+import { useState } from "react"
+import useStore from "../../hooks/stores/useStore"
+import { Activity } from "../../models/activity/types"
 import { variantMessage } from "../../models/activity/utils"
+import { EditCommentForm } from "../comment/EditCommentForm"
 import { AvatarAddress } from "./AvatarAddress"
 
 const ActivityItem = ({
-  accountAddress,
-  variant,
-  createdAt,
-  comment,
+  activity,
+  optimisticEditComment,
 }: {
-  accountAddress: string
-  variant: ActivityVariant
-  createdAt: Date
-  comment?: string
+  activity: Activity
+  optimisticEditComment: any
 }) => {
+  const activeUser = useStore((state) => state.activeUser)
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-row items-center space-x-2">
-        <AvatarAddress size="xs" address={accountAddress} />
+        <AvatarAddress size="xs" address={activity.address} />
         <p className="text-xs text-slate-500">
-          {variantMessage(variant, createdAt)}
+          {variantMessage(activity.variant, activity.createdAt)}
         </p>
       </div>
-      {comment && (
-        <div className="ml-6 mt-1 whitespace-pre-line rounded-md bg-slate-100 px-3 py-2 text-sm">
-          {comment}
+      {activity.data.comment && (
+        <div className="mt-1 ml-6 rounded-md bg-slate-50 px-3 py-2">
+          {isEditing ? (
+            <EditCommentForm
+              optimisticEditComment={optimisticEditComment}
+              activityId={activity.id}
+              initialValue={activity.data.comment}
+              close={() => setIsEditing(false)}
+            />
+          ) : (
+            <>
+              <div className="items-top flex flex-row space-x-2.5">
+                <span className="w-full whitespace-pre-line text-sm">
+                  {activity.data.comment}
+                </span>
+                {activity.address === activeUser?.address && (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="h-4 w-4 rounded-sm hover:cursor-pointer hover:bg-slate-100 data-[state=open]:bg-slate-100">
+                      <EllipsisHorizontalIcon height={16} width={16} />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem onClick={() => setIsEditing(true)}>
+                        Edit
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                )}
+              </div>
+              {activity.data.edited && (
+                <div className="mt-2.5 text-xs text-slate-500">(Edited)</div>
+              )}
+            </>
+          )}
         </div>
       )}
     </div>
