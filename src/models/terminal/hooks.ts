ada16ca06a13cf2ebd77d3715119bf9841dc6a45
kristencheung <krcheung9@gmail.com>
2023-03-12 23:07:58 -0400

    fix nonce check

--- src/models/terminal/hooks.ts
+++ src/models/terminal/hooks.ts
@@ -34,9 +34,41 @@ export const useTerminalByChainIdAndSafeAddress = (
 }
 
 export const useUpdateTerminal = (address: string, chainId: number) => {
+  const { authToken } = useDynamicContext()
   const fetcher = async (url: string, { arg }: { arg: any }) => {
+    const {
+      safeAddress,
+      name,
+      chainId,
+      description,
+      url: descriptionUrl,
+      safeTxnHash,
+      nonce,
+    } = arg
+
+    if (!safeAddress || !name || !chainId)
+      throw Error(
+        `Missing args in "createTerminal". Args specified - safeAddress: ${safeAddress}, chainId: ${chainId}, name: ${name} `,
+      )
+
     try {
-      const response = await axios.post<any>(url, arg)
+      const response = await axios.put<any>(
+        url,
+        {
+          safeAddress,
+          name,
+          chainId,
+          description,
+          url: descriptionUrl,
+          safeTxnHash,
+          nonce,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        },
+      )
       if (response.status === 200) {
         return response.data
       }
@@ -46,7 +78,7 @@ export const useUpdateTerminal = (address: string, chainId: number) => {
   }
 
   const { trigger: updateTerminal, isMutating } = useSWRMutation(
-    `/api/v1/terminal/update?safeAddress=${address}&chainId=${chainId}`,
+    `/api/v1/terminal/${chainId}/${address}`,
     fetcher,
   )
 
