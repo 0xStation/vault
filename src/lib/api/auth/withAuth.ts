8f35b5608102266d70904bdbc714f3daad6ce19a
Kristen Cheung <krcheung9@gmail.com>
2023-03-13 23:33:27 -0400

    add wallet and withAuth type fix (#93)

--- src/lib/api/auth/withAuth.ts
+++ src/lib/api/auth/withAuth.ts
@@ -1,18 +1,19 @@
 import { isAuthenticated } from "lib/api/auth/isAuthenticated"
 import { NextApiRequest, NextApiResponse } from "next"
+import { Ctx } from "./types"
 
 export const withAuth = (
   handler: (
     req: NextApiRequest,
     res: NextApiResponse,
-    ctx?: any,
+    ctx: Ctx,
   ) => Promise<string | void | any>,
 ) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const ctx = await isAuthenticated(req, res)
 
     // route handler
-    await handler(req, res, ctx)
+    await handler(req, res, ctx as Ctx)
   }
 }
 export default withAuth
