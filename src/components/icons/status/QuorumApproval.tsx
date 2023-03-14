04b61438f9be3246ab44ff799971bfb45ef904af
Michael Gingras <mcg79@cornell.edu>
2023-03-14 14:13:16 -0600

    dark mode icons

--- src/components/icons/status/QuorumApproval.tsx
+++ src/components/icons/status/QuorumApproval.tsx
@@ -8,7 +8,7 @@ export const QuorumApproval = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle cx="8" cy="8" r="8" fill="#AD72FF" />
-      <circle cx="8" cy="8" r="6" fill="white" />
+      <circle cx="8" cy="8" r="6" fill="black" />
       <path
         d="M8 8C8 10.7614 8 10.5 8 13C10.7614 13 13 10.7614 13 8C13 5.23858 10.7614 3 8 3C8 4.5 8 5.23858 8 8Z"
         fill="#AD72FF"
