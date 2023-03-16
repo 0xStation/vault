b9f00bb3146561f3d7e2944df1f073c78cdab7d9
Conner Swenberg <cls364@cornell.edu>
2023-03-15 22:01:34 -0400

    Fix empty state box contents spacing

--- src/components/emptyStates/EmptyState.tsx
+++ src/components/emptyStates/EmptyState.tsx
@@ -1,17 +1,17 @@
 export const EmptyState = ({
   title,
   subtitle,
+  children,
 }: {
   title: string
   subtitle: string
+  children?: any
 }) => {
   return (
-    // <div className="h-[calc(100%-36px)]">
-    <div className="mx-4 mb-4 w-full grow content-center rounded-xl bg-gray-90 text-center">
-      <div className="h-2/5"></div>
+    <div className="mx-4 mb-4 flex w-full grow flex-col justify-center rounded-xl bg-gray-90 px-[10%] text-center">
       <div className="text-xl font-bold">{title}</div>
       <div className="mt-2 text-sm">{subtitle}</div>
+      {children}
     </div>
-    // </div>
   )
 }
