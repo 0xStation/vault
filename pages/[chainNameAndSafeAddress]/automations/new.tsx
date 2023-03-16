f0c8a3eaeb830f9efe1b3a0cc7921c817fb8d3a9
Conner Swenberg <cls364@cornell.edu>
2023-03-16 13:12:20 -0500

    Remove unneeded navs from mobile automation creation page

--- pages/[chainNameAndSafeAddress]/automations/new.tsx
+++ pages/[chainNameAndSafeAddress]/automations/new.tsx
@@ -1,23 +1,7 @@
-import { ArrowLeft } from "@icons"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import AccountNavBar from "../../../src/components/core/AccountNavBar"
 import NewAutomationContent from "../../../src/components/pages/newAutomation/components/NewAutomationContent"
 
 const NewAutomationPage = () => {
-  const router = useRouter()
-  return (
-    <>
-      <AccountNavBar />
-      <Link
-        href={`/${router.query.chainNameAndSafeAddress}`}
-        className="block w-fit px-4"
-      >
-        <ArrowLeft />
-      </Link>
-      <NewAutomationContent />
-    </>
-  )
+  return <NewAutomationContent />
 }
 
 export default NewAutomationPage
