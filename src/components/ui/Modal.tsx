e371a08123c0a16ae60a9322fff39db9bc6a56a1
kristencheung <krcheung9@gmail.com>
2023-03-16 16:49:04 -0400

    fix spacing, opacity, etc

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -26,7 +26,7 @@ export default function Modal({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-70" />
+          <div className="fixed inset-0 bg-black bg-opacity-80" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
