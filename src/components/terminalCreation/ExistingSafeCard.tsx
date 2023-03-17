e0713ac5c3fd50f7ce02ac90e23caea73f8b78b0
Kristen Cheung <krcheung9@gmail.com>
2023-03-16 22:28:08 -0400

    safe import component (#145)

--- src/components/terminalCreation/ExistingSafeCard.tsx
+++ src/components/terminalCreation/ExistingSafeCard.tsx
@@ -1,23 +1,33 @@
+import LoadingSpinner from "@ui/LoadingSpinner"
+import { Network } from "@ui/Network"
 import truncateString from "lib/utils"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
 import { VIEW } from "../../../pages/project/new"
+import { useSafeMetadata } from "../../hooks/safe/useSafeMetadata"
 import { useTerminalCreationStore } from "../../hooks/stores/useTerminalCreationStore"
 import SelectorCard from "../core/SelectorCard"
 
 export const ExistingSafeCard = ({
+  chainId,
   safeAddress,
   setView,
-  setSelectedAddress,
-  setDrawerOpen,
 }: {
   chainId: number
   safeAddress: string
   setView: Dispatch<SetStateAction<VIEW.CREATION_OPTIONS | VIEW.CREATE_FORM>>
-  setSelectedAddress: Dispatch<SetStateAction<string>>
-  setDrawerOpen: Dispatch<SetStateAction<boolean>>
 }) => {
+  const [selectedAddress, setSelectedAddress] = useState<string>("")
   const setFormData = useTerminalCreationStore((state) => state.setFormData)
   const formData = useTerminalCreationStore((state) => state.formData)
+  const { safeMetadata, isLoading: isSafeMetadataLoading } = useSafeMetadata({
+    address: selectedAddress,
+    chainId: chainId,
+  })
+
+  const plural =
+    safeMetadata?.signers?.length && safeMetadata?.signers?.length === 1
+      ? ""
+      : "s"
 
   return (
     <SelectorCard
@@ -27,18 +37,27 @@ export const ExistingSafeCard = ({
         setView(VIEW.CREATE_FORM)
       }}
     >
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-col">
         <p className="font-bold">{truncateString(safeAddress)}</p>
-        <button
-          className="text-sm text-violet"
-          onClick={(e) => {
-            e.stopPropagation()
-            setSelectedAddress(safeAddress)
-            setDrawerOpen(true)
-          }}
-        >
-          View details
-        </button>
+        {isSafeMetadataLoading ? (
+          <LoadingSpinner />
+        ) : safeMetadata ? (
+          <p className="mt-1 flex flex-row text-sm text-gray">
+            <Network chainId={chainId} />
+            &nbsp;· {safeMetadata?.signers?.length}
+            &nbsp;member{plural}
+          </p>
+        ) : (
+          <button
+            className="w-fit text-sm text-violet"
+            onClick={(e) => {
+              e.stopPropagation()
+              setSelectedAddress(safeAddress)
+            }}
+          >
+            View details
+          </button>
+        )}
       </div>
     </SelectorCard>
   )
