c9a0d75c91b43b420aca1c12f6ecf0ab6c785e5b
paprika <apivessa.mind@gmail.com>
2023-03-17 03:39:22 -0400

    polish tokentransferrequestcard

--- src/components/request/TokenTransferRequestCard.tsx
+++ src/components/request/TokenTransferRequestCard.tsx
@@ -15,34 +15,38 @@ export const TokenTransferRequestCard = ({
   const [showRecipients, setShowRecipients] = useState<boolean>(true)
   let tokenTransferMeta = request.data.meta as TokenTransferVariant
   return (
-    <div className="space-y-4 rounded bg-gray-90 p-4">
-      <span className="text-sm text-gray">#{request.number}</span>
-      <div
-        className="flex flex-row items-center justify-between"
-        onClick={() => setShowRecipients(!showRecipients)}
-      >
-        <span className="text-sm text-gray">Recipient</span>
-        {showRecipients ? (
-          <ChevronDownIcon className="ml-auto h-4 w-4" />
-        ) : (
-          <ChevronRightIcon className="ml-auto h-4 w-4" />
+    <div className="space-y-4 rounded bg-gray-100 p-4">
+      <div className="space-y-2">
+        <span className="text-sm text-gray">#{request.number}</span>
+        <div
+          className="flex flex-row items-center justify-between"
+          onClick={() => setShowRecipients(!showRecipients)}
+        >
+          <span className="text-sm text-gray">Recipient</span>
+          {showRecipients ? (
+            <ChevronDownIcon className="ml-auto h-4 w-4" />
+          ) : (
+            <ChevronRightIcon className="ml-auto h-4 w-4" />
+          )}
+        </div>
+        {showRecipients && (
+          <div className="mt-2 items-center rounded-lg bg-gray-90 p-2">
+            <AvatarAddress
+              className="mt-1"
+              size="sm"
+              address={tokenTransferMeta.recipient}
+            />
+          </div>
         )}
       </div>
-      {showRecipients && (
-        <div className="mt-2 items-center rounded-lg bg-gray-80 p-2">
-          <AvatarAddress
-            className="mt-1"
-            size="sm"
-            address={tokenTransferMeta.recipient}
-          />
-        </div>
-      )}
-      <TokenTransfersAccordion
-        transfers={tokenTransferMeta.transfers}
-        transferBgGray={false}
-      />
-      <span className="block text-sm text-gray">Note</span>
-      <div>{request.data.note}</div>
+      <div className="space-y-2">
+        <TokenTransfersAccordion
+          transfers={tokenTransferMeta.transfers}
+          transferBgGray={false}
+        />
+        <span className="block text-sm text-gray">Note</span>
+        <div>{request.data.note}</div>
+      </div>
     </div>
   )
 }
