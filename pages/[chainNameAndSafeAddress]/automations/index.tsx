cd00eb406fc6c6f26ff8bad94b0987372a59b509
Kristen Cheung <krcheung9@gmail.com>
2023-03-17 02:03:27 -0400

    hide automations cta (#149)

--- pages/[chainNameAndSafeAddress]/automations/index.tsx
+++ pages/[chainNameAndSafeAddress]/automations/index.tsx
@@ -1,8 +1,16 @@
 import Breakpoint from "@ui/Breakpoint"
+import { convertGlobalId } from "models/terminal/utils"
+import { useRouter } from "next/router"
 import Desktop from "../../../src/components/pages/automations/Desktop"
 import Mobile from "../../../src/components/pages/automations/Mobile"
+import { useIsSigner } from "../../../src/hooks/useIsSigner"
 
 const AutomationsPage = () => {
+  const router = useRouter()
+  const { chainId, address } = convertGlobalId(
+    router.query.chainNameAndSafeAddress as string,
+  )
+  useIsSigner({ chainId: chainId as number, address: address as string })
   return (
     <Breakpoint>
       {(isMobile) => {
