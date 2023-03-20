e010075a98c33265c2a8779d943dde27e6ed255b
paprika <apivessa.mind@gmail.com>
2023-03-20 15:46:01 -0400

    profile

--- pages/u/[address]/profile.tsx
+++ pages/u/[address]/profile.tsx
@@ -1,17 +1,34 @@
 import Breakpoint from "@ui/Breakpoint"
+import { useTerminalByChainIdAndSafeAddress } from "models/terminal/hooks/useTerminalByChainIdAndSafeAddress"
+import { convertGlobalId } from "models/terminal/utils"
+import Head from "next/head"
+import { useRouter } from "next/router"
 import Desktop from "../../../src/components/pages/profile/Desktop"
 import Mobile from "../../../src/components/pages/profile/Mobile"
 
 const ProfilePage = ({}: {}) => {
+  const router = useRouter()
+  const { chainId, address } = convertGlobalId(
+    router.query.chainNameAndSafeAddress as string,
+  )
+  const { terminal } = useTerminalByChainIdAndSafeAddress(
+    address as string,
+    chainId as number,
+  )
   return (
-    <Breakpoint>
-      {(isMobile) => {
-        if (isMobile) {
-          return <Mobile />
-        }
-        return <Desktop />
-      }}
-    </Breakpoint>
+    <>
+      <Head>
+        <title>Proposals | {terminal?.data.name}</title>
+      </Head>
+      <Breakpoint>
+        {(isMobile) => {
+          if (isMobile) {
+            return <Mobile />
+          }
+          return <Desktop />
+        }}
+      </Breakpoint>
+    </>
   )
 }
 
