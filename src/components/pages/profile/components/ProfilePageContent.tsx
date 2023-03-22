bce934a143eebd755987b94c91155b4f114271ef
kristencheung <krcheung9@gmail.com>
2023-03-22 02:05:11 -0400

    move items to clam to sidebar

--- src/components/pages/profile/components/ProfilePageContent.tsx
+++ src/components/pages/profile/components/ProfilePageContent.tsx
@@ -1,6 +1,4 @@
-import { useDynamicContext } from "@dynamic-labs/sdk-react"
 import { useRouter } from "next/router"
-import { ProfileReadyToClaim } from "../../../claim/ProfileReadyToClaim"
 import ProfileTabBar from "../../../core/TabBars/ProfileTabBar"
 import { ProfileRequestsList } from "../../../request/ProfileRequestsList"
 import { ProfileTerminalsList } from "../../../terminal/ProfileTerminalsList"
@@ -8,22 +6,9 @@ import { ProfileTerminalsList } from "../../../terminal/ProfileTerminalsList"
 const ProfilePageContent = ({}: {}) => {
   const router = useRouter()
   const accountAddress = router.query.address as string
-  const { primaryWallet } = useDynamicContext()
-  const isUsersProfile =
-    primaryWallet?.address &&
-    accountAddress &&
-    accountAddress === primaryWallet?.address
 
   return (
     <div className="flex h-full grow flex-col pb-4">
-      {/* ACCOUNT */}
-      {isUsersProfile && (
-        <div className="mt-6 space-y-3 px-4">
-          <div>
-            <ProfileReadyToClaim />
-          </div>
-        </div>
-      )}
       {/* TABS */}
       <ProfileTabBar className="mt-6">
         <ProfileTerminalsList address={accountAddress} />
