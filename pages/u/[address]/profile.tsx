53ffe1646315f1c3ce3d493ba4dc0fe7d3521c23
kristencheung <krcheung9@gmail.com>
2023-03-22 03:11:57 -0400

    use ens in page title

--- pages/u/[address]/profile.tsx
+++ pages/u/[address]/profile.tsx
@@ -1,16 +1,28 @@
 import Breakpoint from "@ui/Breakpoint"
+import truncateString from "lib/utils"
 import Head from "next/head"
 import { useRouter } from "next/router"
+import { useEnsName } from "wagmi"
 import Desktop from "../../../src/components/pages/profile/Desktop"
 import Mobile from "../../../src/components/pages/profile/Mobile"
 
 const ProfilePage = ({}: {}) => {
   const router = useRouter()
+  const { data: ensName } = useEnsName({
+    address: router.query?.address as `0x${string}`,
+    chainId: 1,
+    cacheTime: 60 * 60 * 1000, // (1 hr) time (in ms) which the data should remain in the cache
+  })
 
   return (
     <>
       <Head>
-        <title>Profile | {router?.query?.address}</title>
+        <title>
+          Profile |{" "}
+          {ensName ||
+            (router?.query?.address &&
+              truncateString(router.query?.address as string))}
+        </title>
       </Head>
       <Breakpoint>
         {(isMobile) => {
