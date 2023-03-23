56f8828a7c813b4b198e4c757029cb414486ac1a
Michael Gingras <mcg79@cornell.edu>
2023-03-23 12:36:57 -0600

    slight animation on logo hover (#208)

--- src/components/core/AccountNavBar/index.tsx
+++ src/components/core/AccountNavBar/index.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image"
+import Link from "next/link"
 import React from "react"
+import { useAccount } from "wagmi"
 import AccountDropdown from "./AccountDropdown"
 
 export const AccountNavBar = () => {
+  const { address: accountAddress } = useAccount()
   return (
     <nav className="flex w-full flex-row items-center justify-between px-4 pt-12 pb-3">
-      <Image src="/images/terminal-logo.webp" alt="" height={40} width={40} />
+      <div className="hover:rotate-12 hover:animate-bounce">
+        <Link href={`/u/${accountAddress}/profile`}>
+          <Image
+            src="/images/terminal-logo.webp"
+            alt=""
+            height={40}
+            width={40}
+          />
+        </Link>
+      </div>
       <AccountDropdown />
     </nav>
   )
