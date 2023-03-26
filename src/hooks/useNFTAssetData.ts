2b4a5be448fd1aabbe7947fd876e9acc6558b671
Conner Swenberg <cls364@cornell.edu>
2023-03-26 00:21:25 -0400

    Fix SVG support and all image domains

--- src/hooks/useNFTAssetData.ts
+++ src/hooks/useNFTAssetData.ts
@@ -11,8 +11,12 @@ const useNFTAssetData = (address: string, chainId: number) => {
   }
 
   const endpoint = `https://api.n.xyz/api/v1/address/${address}/balances/nfts?chainID=${nChainIdToChainName[chainId]}&apikey=${process.env.NEXT_PUBLIC_N_XYZ_API_KEY}`
+  const enabled = !!address && !!chainId
 
-  const { isLoading, data, mutate, error } = useSWR(endpoint, fetcher)
+  const { isLoading, data, mutate, error } = useSWR(
+    enabled ? endpoint : null,
+    fetcher,
+  )
 
   return { data, isLoading, error, mutate }
 }
