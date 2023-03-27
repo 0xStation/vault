85fa215f7e7fc43f978a3a515cf72ffa81e7c8a8
Kristen Cheung <krcheung9@gmail.com>
2023-03-27 14:39:02 -0400

    remove account disconnecting (#217)

--- src/components/core/AppLayout.tsx
+++ src/components/core/AppLayout.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head"
-import { ReactNode, useEffect } from "react"
-import { useAccount, useDisconnect } from "wagmi"
+import { ReactNode } from "react"
 import { useSetActiveUser } from "../../hooks/account/useSetActiveUser"
-import useStore from "../../hooks/stores/useStore"
 import { Toast } from "../ui/Toast"
 
 export const AppLayout = ({
@@ -12,28 +10,7 @@ export const AppLayout = ({
   title?: string
   children: ReactNode
 }) => {
-  const setActiveUser = useStore((state) => state.setActiveUser)
-  const activeUser = useStore((state) => state.activeUser)
-  const { address: connectedAddress } = useAccount()
-  const { disconnect } = useDisconnect()
-
   useSetActiveUser()
-
-  useEffect(() => {
-    // log user out / disconnect them if they
-    // programatically disconnect from their wallet extension
-    // or if they switch accounts within their own wallet.
-    const handleDisconnect = async () => {
-      if (
-        !connectedAddress ||
-        (activeUser?.address && activeUser?.address !== connectedAddress)
-      ) {
-        setActiveUser(null)
-        disconnect()
-      }
-    }
-    handleDisconnect()
-  }, [connectedAddress])
 
   return (
     <>
