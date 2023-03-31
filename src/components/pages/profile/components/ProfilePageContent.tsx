cecbb18181604643b1b096872fcc73ae35bb4e61
Kristen Cheung <krcheung9@gmail.com>
2023-03-31 13:37:55 -0400

    claim items on mobile (#228)

--- src/components/pages/profile/components/ProfilePageContent.tsx
+++ src/components/pages/profile/components/ProfilePageContent.tsx
@@ -1,3 +1,5 @@
+import { useDynamicContext } from "@dynamic-labs/sdk-react"
+import { ProfileReadyToClaim } from "components/claim/ProfileReadyToClaim"
 import { useRouter } from "next/router"
 import ProfileTabBar from "../../../core/TabBars/ProfileTabBar"
 import { ProfileRequestsList } from "../../../request/ProfileRequestsList"
@@ -6,9 +8,22 @@ import { ProfileTerminalsList } from "../../../terminal/ProfileTerminalsList"
 const ProfilePageContent = ({}: {}) => {
   const router = useRouter()
   const accountAddress = router.query.address as string
+  const { primaryWallet } = useDynamicContext()
+  const isUsersProfile =
+    primaryWallet?.address &&
+    accountAddress &&
+    accountAddress === primaryWallet?.address
 
   return (
     <div className="flex h-full grow flex-col pb-4">
+      {/* ACCOUNT */}
+      {isUsersProfile && (
+        <div className="mt-6 block space-y-3 px-4 md:hidden">
+          <div>
+            <ProfileReadyToClaim />
+          </div>
+        </div>
+      )}
       {/* TABS */}
       <ProfileTabBar className="mt-6">
         <ProfileTerminalsList address={accountAddress} />
